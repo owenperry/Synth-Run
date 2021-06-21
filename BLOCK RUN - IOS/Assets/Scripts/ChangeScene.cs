687bc469fbd45cc78dfed0f1584dfcaa4b14812a
maccap <60553783+maccap007@users.noreply.github.com>
2021-06-21 20:56:58 +0100

    title changes

--- BLOCK RUN - IOS/Assets/Scripts/ChangeScene.cs
+++ BLOCK RUN - IOS/Assets/Scripts/ChangeScene.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 public class ChangeScene : MonoBehaviour
 {
     public GameObject music;
-    public void btn_change_scene(string scene_name)
+    public void btn_change_scene()
     {
-        string scnName = scene_name;
         Debug.Log("Clicked");
-        SceneManager.LoadScene(scene_name);
+        SceneManager.LoadScene(1);
     }
 }
