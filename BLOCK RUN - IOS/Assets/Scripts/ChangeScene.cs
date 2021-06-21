599955ef2a871225e98922836c32b8e3ff14da50
owenperry <58910182+owenperry@users.noreply.github.com>
2021-06-21 16:22:59 +0100

    Uploading the unity folder to the repo

---
+++ BLOCK RUN - IOS/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    public GameObject music;
+    public void btn_change_scene(string scene_name)
+    {
+        string scnName = scene_name;
+        Debug.Log("Clicked");
+        SceneManager.LoadScene(scene_name);
+    }
+}
---
+++ BLOCK RUN - IOS/Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+
+    public float distance = 0;
+    public string scoreCount;
+    public string percentage = "%";
+
+    public GameObject object1;
+    public GameObject object2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (object2.transform.position.z - object1.transform.position.z);
+
+        if (distance >= 1)
+        {
+            scoreCount = (100 - (distance / 30)).ToString("0");
+            scoreText.text = scoreCount + percentage;
+        }
+    }
+
+}
